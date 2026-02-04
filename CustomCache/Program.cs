23c691f27553c035e8618266883fbdaa6dfac39d
OmarMTDev <omarmtdev@gmail.com>
2026-02-03 22:35:45 -0600

    Applying Open-Closed principle and Decorator DP

--- CustomCache/Program.cs
+++ CustomCache/Program.cs
@@ -1,19 +1,19 @@
-﻿Downloader dw = new ();
+﻿IDataDownloader dw = new CachingDownloader(new Downloader());
 
-Console.WriteLine(dw.CachedDownloadData("32da-224ef-124a-ab12"));
-Console.WriteLine(dw.CachedDownloadData("ab12-32da-224ef-124a"));
-Console.WriteLine(dw.CachedDownloadData("124a-32da-224ef-ab12"));
+Console.WriteLine(dw.SlowDownloadData("32da-224ef-124a-ab12"));
+Console.WriteLine(dw.SlowDownloadData("ab12-32da-224ef-124a"));
+Console.WriteLine(dw.SlowDownloadData("124a-32da-224ef-ab12"));
 
 System.Console.WriteLine("CACHED");
-Console.WriteLine(dw.CachedDownloadData("32da-224ef-124a-ab12"));
-Console.WriteLine(dw.CachedDownloadData("ab12-32da-224ef-124a"));
+Console.WriteLine(dw.SlowDownloadData("32da-224ef-124a-ab12"));
+Console.WriteLine(dw.SlowDownloadData("ab12-32da-224ef-124a"));
 
 
 public class Cache<TKey, TData>
 {
     Dictionary<TKey, TData> _cachedData = new();
 
-    public TData Get(TKey key, Func<TKey,TData> getInitialData)
+    public TData Get(TKey key, Func<TKey, TData> getInitialData)
     {
         if (!_cachedData.ContainsKey(key))
         {
@@ -23,20 +23,33 @@ public class Cache<TKey, TData>
     }
 }
 
-public class Downloader
+public interface IDataDownloader
 {
-    private readonly Cache<string, string> _cachedData = new();
+    public string SlowDownloadData(string id);
+}
 
-    public string CachedDownloadData(string id)
+public class CachingDownloader : IDataDownloader
+{
+    private readonly IDataDownloader _dataDownloader;
+    private readonly Cache<string, string> _cache = new();
+
+    public CachingDownloader(IDataDownloader dataDownloader)
     {
-    
-       return _cachedData.Get(id, SlowDownloadData);
-
+        _dataDownloader = dataDownloader;
     }
+
+    public string SlowDownloadData(string id)
+    {
+        return _cache.Get(id, _dataDownloader.SlowDownloadData);
+    }
+}
+
+public class Downloader : IDataDownloader
+{
     public string SlowDownloadData(string id)
     {
         Thread.Sleep(1000);
-       return $"Returning data with id: {id}";
+        return $"Returning data with id: {id}";
 
     }
 }
