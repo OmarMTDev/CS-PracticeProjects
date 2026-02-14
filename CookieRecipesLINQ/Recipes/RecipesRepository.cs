93186cedbef628ef867035d756c1f6bcb2994113
OmarMTDev <omarmtdev@gmail.com>
2026-02-14 13:58:57 -0600

    Refactoring the RecipesRepository

--- CookieRecipesLINQ/Recipes/RecipesRepository.cs
+++ CookieRecipesLINQ/Recipes/RecipesRepository.cs
@@ -33,32 +33,34 @@ public class RecipesRepository : IRecipesRepository
 
     private Recipe RecipeFromString(string recipeFromFile)
     {
-        var textualIds = recipeFromFile.Split(Separator);
-        var ingredients = new List<Ingredient>();
-
-        foreach (var textualId in textualIds)
-        {
-            var id = int.Parse(textualId);
-            var ingredient = _ingredientsRegister.GetById(id);
-            ingredients.Add(ingredient);
-        }
+        var ingredients = recipeFromFile.Split(Separator)
+          .Select(int.Parse)
+          .Select(_ingredientsRegister.GetById);
 
         return new Recipe(ingredients);
     }
 
     public void Write(string filePath, List<Recipe> allRecipes)
     {
-        var recipesAsStrings = new List<string>();
-        foreach (var recipe in allRecipes)
-        {
-            var allIds = new List<int>();
-            foreach (var ingredient in recipe.Ingredients)
-            {
-                allIds.Add(ingredient.Id);
-            }
-            recipesAsStrings.Add(string.Join(Separator, allIds));
-        }
+        /* Simpler
+        var recipesAsStrings = allRecipes
+        .Select(recipe => string.Join(
+            Separator, recipe.Ingredients.Select(ingredient => ingredient.Id))
+        );
+        */
 
-        _stringsRepository.Write(filePath, recipesAsStrings);
+        //Better readability
+        var recipesAsStrings = allRecipes
+        .Select(recipe =>
+       {
+           var allIds = recipe.Ingredients
+           .Select(ingredient => ingredient.Id);
+           return string.Join(
+            Separator, allIds);
+       });
+
+
+
+        _stringsRepository.Write(filePath, recipesAsStrings.ToList());
     }
 }
